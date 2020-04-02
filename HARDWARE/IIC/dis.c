48e5eb96ebbaf007b6d43d4a98f36fc325448011
qileiONE <1114514990@qq.com>
2020-04-02 15:16:19 +0800

    PCF8596 driver

--- HARDWARE/IIC/dis.c
+++ HARDWARE/IIC/dis.c
@@ -4,6 +4,66 @@
 #include "stdio.h"
 
 
+void PCF8576_WriteCommand(unsigned char command)
+{
+	IIC_Start();                  
+	IIC_Send_Byte(PCF8576);                
+	IIC_Wait_Ack();                     
+	IIC_Send_Byte(command);                
+	IIC_Wait_Ack();
+	IIC_Stop();                  
+}
 
+void PCF8576_WritePoint(unsigned char suba,unsigned char dat)
+{
+	IIC_Start();  
+	IIC_Send_Byte(PCF8576);  
+	IIC_Wait_Ack();       
+	IIC_Send_Byte(suba);   
+	IIC_Wait_Ack();  
+	IIC_Send_Byte(dat);   
+	IIC_Wait_Ack(); 
+	IIC_Stop();
+}
+
+void PCF8576_WriteStr(unsigned char suba,unsigned char *str,unsigned char no)
+{
+	unsigned char i;
+	IIC_Start();  
+	IIC_Send_Byte(PCF8576);  
+	IIC_Wait_Ack();       
+	IIC_Send_Byte(suba);   
+	IIC_Wait_Ack();  
+	
+	for(i=0;i<no;i++)
+	{
+		IIC_Send_Byte(*str);   
+		IIC_Wait_Ack(); 
+		str++;
+	}
+	IIC_Stop();
+}
+
+void PCF8576_Ini(unsigned char mode,unsigned char glitter)
+{
+	mode = (mode&0x1f)|0x40;
+	PCF8576_WriteCommand(mode);
+	glitter = (glitter&0x07)|0x70;
+	PCF8576_WriteCommand(glitter);
+}
+
+void PCF8576_Point(unsigned addr,unsigned char dat)
+{
+	PCF8576_WriteCommand(0x60);
+	addr = addr&0x3f;
+	PCF8576_WritePoint(addr,dat);
+}
+
+void PCF8576_Disp(unsigned addr,unsigned char *str,unsigned char no)
+{
+	PCF8576_WriteCommand(0x60);
+	addr = addr&0x3f;
+	PCF8576_WriteStr(addr,str,no);
+}
 
 
--- HARDWARE/IIC/dis.h
+++ HARDWARE/IIC/dis.h
@@ -1,6 +1,24 @@
 #ifndef __DIS_H
 #define __DIS_H
 #include "sys.h" 
+ 
+#define MODE_LP		0x10 
+#define MODE_E  	0x08 
+#define MODE_B  	0x04 
+#define MODE_M1 	0x02 
+#define MODE_M0     0x01 
+ 
+#define GLITTER_A   0x04 
+#define GLITTER_BF1 0x02 
+#define GLITTER_BF0 0x01 
+ 
+#define NOGLITTER   0x00 
+ 
+#define PCF8576 0x70          	/* Æ÷¼þµØÖ· */ 
 
+
+void PCF8576_Ini(unsigned char mode,unsigned char glitter);
+void PCF8576_Point(unsigned addr,unsigned char dat);
+void PCF8576_Disp(unsigned addr,unsigned char *str,unsigned char no);
 
 #endif
